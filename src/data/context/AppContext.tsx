c1a860bc211d46ac65d7cea592e43f9a9bb330e6
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-03 10:34:28 -0300

    Add context api

---
+++ src/data/context/AppContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode, useState } from "react";
+
+type Theme = 'dark' | ''
+
+type AppContextData={
+    theme:Theme
+    switchTheme:()=>void
+}
+
+type AppProviderProps = {
+    children:ReactNode
+}
+
+export function AppProvider(props:AppProviderProps){
+    const [theme,setTheme] = useState<Theme>('')
+    
+    function switchTheme(){
+        setTheme(theme === '' ? 'dark':'')
+    }
+
+    return(
+        <AppContext.Provider value={{
+            theme,
+            switchTheme,
+
+        }}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+const AppContext = createContext({} as AppContextData)
+
+export default AppContext
+
+export const AppConsume = AppContext.Consumer
--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,9 +1,13 @@
 import Layout from "../components/template/Layout";
+import { AppConsume } from "../data/context/AppContext";
+import useAppData from "../data/hook/useAppData";
 
 export default function News(){
+    const data = useAppData()
+
     return(
         <Layout title="News" subtitle="Creating template news">
-            <h3>News Content</h3>
+            <h3>News content</h3>
         </Layout>
     )
 }
