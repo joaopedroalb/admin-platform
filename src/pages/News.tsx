990e8f95b367da4c8084c2dbf2f88f1e615d72f4
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-03 10:38:18 -0300

    Add theme default in layout

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,10 +1,6 @@
 import Layout from "../components/template/Layout";
-import { AppConsume } from "../data/context/AppContext";
-import useAppData from "../data/hook/useAppData";
 
 export default function News(){
-    const data = useAppData()
-
     return(
         <Layout title="News" subtitle="Creating template news">
             <h3>News content</h3>
