1a392a2693f5464af454afa70b6ca261002cea3b
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-03 12:49:57 -0300

    Update style btnTheme component

--- src/components/template/BtnTheme.tsx
+++ src/components/template/BtnTheme.tsx
@@ -7,20 +7,42 @@ export default function BtnTheme(){
 
     return theme==='dark'?(
         <div onClick={switchTheme} className={`
-        
+            hidden sm:flex items-center cursor-pointer
+            bg-gradient-to-r from-yellow-300 to-yellow-600
+            w-14 lg:w-24 h-8 p-1 rounded-full
         `}>
             <div className={`
-        
+                flex items-center justify-center
+              text-yellow-600 text-yellow-600 bg-white
+                w-6 h-6 rounded-full
             `}>
-                {IconSun}
+                {IconSun(4)}
+            </div>
+            <div className={`
+                hidden lg:flex items-center ml-2
+                text-white
+            `}>
+                <span>Ligth</span>
             </div>
         </div>
     ):(
         <div onClick={switchTheme} className={`
-        
+            hidden sm:flex items-center justify-end cursor-pointer 
+            bg-gradient-to-r from-gray-500 to-gray-900
+            w-14 lg:w-24 h-8 p-1 rounded-full
         `}>
-            <div>
-                {IconMoon}
+            <div className={`
+                hidden lg:flex items-center mr-4 
+                text-gray-300
+            `}>
+                <span>Dark</span>
+            </div>
+            <div className={`
+                flex items-center justify-center
+             text-yellow-300 bg-black
+                w-6 h-6 rounded-full 
+            `}>
+                {IconMoon(4)}
             </div>
         </div>
     )
