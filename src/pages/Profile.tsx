3c1a9a5d6ff4a0807f409ecfe23790752be957c3
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-06 09:54:33 -0300

    Fixed name in profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,8 +5,12 @@ import useAuthData from "../data/hook/useAuthData";
 export default function Profile(){
     const {user} = useAuthData()
     
+    function getName(){
+        return (user?.name != '' && user) ? user?.name: "Usuário"
+    }
+
     return(
-        <Layout title={`Perfil do ${user?.name??"Usuário"}`}
+        <Layout title={`Perfil do ${getName()}`}
                 subtitle="Administre as suas informações ..."        
         >
             <h1>user content</h1>
